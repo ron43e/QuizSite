889e30b978952cbf5fef7972dc372219d2c1951e
ron43e <rosw49@yahoo.com>
2017-02-08 12:35:00 -0500

    Initial commit

---
+++ src/js/directives.js
@@ -0,0 +1,17 @@
+//
+// add / remove 'active' to menu
+app.directive('isActiveNav', ['$location', function ($location) {
+  return {
+    restrict: 'A',
+    link: function (scope, element) {
+      scope.location = $location;
+      scope.$watch('location.path()', function (currentPath) {
+        if ('/#' + currentPath === element[0].attributes['href'].nodeValue) {
+          element.parent().addClass('active');
+        } else {
+          element.parent().removeClass('active');
+        }
+      });
+    }
+  };
+}]);
