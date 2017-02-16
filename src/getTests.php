659099b74ef1a2bc1ecb15facd6827eb2dfd2d29
ron43e <rosw49@yahoo.com>
2017-02-16 11:37:31 -0500

    Prior to adding a check to see if logged in

---
+++ src/getTests.php
@@ -0,0 +1,41 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+$jh = fopen("err.txt", "w");
+fwrite($jh, "begin\r\n");
+
+$conn = new mysqli("127.0.0.1", "root", "", "lctests");
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL:";
+}
+//echo $conn->host_info . "\n";
+fwrite($jh, $conn->host_info . "\r\n");
+
+$result = $conn->query("SELECT id, name, minutes_allowed FROM tests");
+//fwrite($jh, 'Result: ' . $result . '\n');
+if ($result == false ) {
+    fwrite($jh, "Result: false\n");
+} else {
+    fwrite($jh, "Result: true;  Rows: " . $result->num_rows . "\n");
+}
+
+$outp = "";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "") {$outp .= ",";}
+    $outp .= '{"name":"'  . $rs["name"] . '",';
+    $outp .= '"id":"'   . $rs["id"]        . '",';
+    $outp .= '"minutes":"'. $rs["minutes_allowed"]     . '"}';
+}
+fwrite($jh, "rs: " . $rs . "\n");
+
+if ($outp != "") {
+    fwrite($jh, "not empty\r\n");
+} else {
+    fwrite($jh, "empty\r\n");
+}
+$outp ='{"records":['.$outp.']}';
+$conn->close();
+
+echo($outp);
+fclose($jh);
+?>
--- src/js/app.js
+++ src/js/app.js
@@ -10,14 +10,20 @@ var config = {
 firebase.initializeApp(config);
 
 
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute'])
+  .run(function ($rootScope) {
+    $rootScope.loggedIn = false;
+  });
 
 app.config(function ($routeProvider, $locationProvider) {
+  //  $locationProvider.html5Mode(true).hashPrefix('*');
   $locationProvider.hashPrefix('');
-  $locationProvider.html5Mode(true);
   $routeProvider
     .when("/", {
       templateUrl: "pages/main.html"
+    })
+    .when("/welcome", {
+      templateUrl: "pages/welcome.html"
     })
     .when("/tests", {
       templateUrl: "pages/tests.html"
@@ -25,34 +31,50 @@ app.config(function ($routeProvider, $locationProvider) {
     .when("/results", {
       templateUrl: "pages/results.html"
     })
-    .when(",/login", {
-      templateUrl: "pages/results.html",
+    .when("/login", {
+      templateUrl: "pages/login.html",
+      controller: "mainCtrl"
     })
-    .when(",/badLogin", {
+    .when("/badLogin", {
       templateUrl: "pages/badLogin.html",
       controller: "mainCtrl"
     })
 });
 
-app.run([
-  '$rootScope',
-  function ($rootScope) {
-    $rootScope.$on('$locationChangeStart', function (event, newUrl, oldUrl) {
-      // both newUrl and oldUrl are strings
-      console.log('Starting to leave %s to go to %s', oldUrl, newUrl);
-    });
-  }
-]);
 
 // create the controller and inject Angular's $scope
-app.controller('mainCtrl', function ($scope, $location) {
-  $scope.loggedIn = false;
+app.controller('mainCtrl', function ($scope, $location, $rootScope, $http) {
   $scope.user = {
     id: '',
     name: 'James',
     email: 'jhindmon@gmail.com',
     password: 'dallas'
   }
+  $scope.tests = [
+    { name: 'Server 1' },
+    { name: 'Server 2' },
+    { name: 'Server 3' },
+    { name: 'Server 4' },
+    { name: 'Server 5' },
+  ]
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+  var yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = '0' + dd
+  }
+  if (mm < 10) {
+    mm = '0' + mm
+  }
+  today = mm + '/' + dd + '/' + yyyy;
+  $scope.results = [
+    { date: today, user: 'jim', test: '2', score: '100' },
+    { date: today, user: 'sam', test: '2', score: '100' },
+    { date: today, user: 'jim', test: '3', score: '94' },
+    { date: today, user: 'jim', test: '4', score: '92' },
+    { date: today, user: 'jim', test: '5', score: '90' },
+  ]
   // create a message to display in our view
   $scope.message = 'Everyone come and see how good I look!';
   // const rootRef = firebase.database().ref().child('angular');
@@ -62,8 +84,9 @@ app.controller('mainCtrl', function ($scope, $location) {
     firebase.auth().signInWithEmailAndPassword($scope.user.email, $scope.user.password)
       .then(function (j) {
         $scope.user.id = j.i; // save auth id
-        $scope.loggedIn = true;
-        $location.path('/results');
+        $rootScope.loggedIn = true;
+        $location.path('/welcome');
+        $scope.$apply();
       })
       .catch(function (error) {
         // Handle Errors here.
@@ -72,16 +95,27 @@ app.controller('mainCtrl', function ($scope, $location) {
         $location.path('/badLogin')
       })
   }
-  $scope.logout = function () {
+  $scope.logOut = function () {
     firebase.auth().signOut().then(function () {
       // Sign-out successful.
-      $scope.loggedIn = false;
+      $rootScope.loggedIn = false;
+      $location.path('/');
+      $scope.$apply();
     }, function (error) {
       // An error happened.
     });
   }
 });
 
-app.controller('aboutCtrl', function ($scope) {
-  $scope.message = 'Look! I am an about page.';
+app.controller('resultsCtrl', function ($scope, $http) {
+  $scope.message = 'Look! I am a results page.';
+  $scope.results = scores;
+//  $http.get("getTests.php")
+//    .then(function (response) {
+//      $scope.results = response.data.records;
+//    });
+  // $scope.results = [
+  //   { test: 'Sv 1', score: 94 },
+  //   { test: 'Sv 2', score: 91 }
+  // ]
 });
